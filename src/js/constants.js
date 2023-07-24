9b451d35b73c1d9bf34a94b8f63838966820a3d3
Patrick McKinney <pmacMaps@users.noreply.github.com>
2023-07-24 16:24:54 -0400

    Update constants.js

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,6 +1,3 @@
-// esri api key
-export const esriKey = 'AAPK56c13fae1a6d4407a5d392981b9b50d2kwxUZMYHbCl1MyifP-JufC7lrC9K2TvvL93oR25h20j0cjvoAro30x-p0O27jPwS';
-
 // dcnr local parks
 export const local_parks_service = "https://www.gis.dcnr.state.pa.us/agsprod/rest/services/BRC/LocalParks/MapServer/1";
 export const local_parks_fields = ['PARK_NAME', 'PREMISE_ADDRESS', 'PREMISE_CITY', 'PREMISE_ZIP', 'Lat_Cen', 'Long_Cen'];
